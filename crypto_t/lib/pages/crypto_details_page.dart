01e53c5ae8d1110713b785234f41b7f8ecff0f2f
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-05-03 21:27:57 +0300

    Create crypto_details_page.dart

---
+++ crypto_t/lib/pages/crypto_details_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CryptoDetailsPage extends StatefulWidget {
+  @override
+  _CryptoDetailsPageState createState() => _CryptoDetailsPageState();
+}
+
+class _CryptoDetailsPageState extends State<CryptoDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
