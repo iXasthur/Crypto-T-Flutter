74e5512be00d0ec6752afe8de64fec4aacda2270
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-05-03 22:28:09 +0300

    Update crypto_details_page.dart

--- crypto_t/lib/pages/crypto_details_page.dart
+++ crypto_t/lib/pages/crypto_details_page.dart
@@ -23,22 +23,18 @@ class _CryptoDetailsPageState extends State<CryptoDetailsPage> {
     var event = widget.asset.suggestedEvent;
 
     return Scaffold(
-      appBar: MyAppBar.createWithAutoBack(
-        context,
-        title: widget.asset.name,
-        onBack: () {
-
-        },
-        actions: [
-          IconButton(
-            onPressed: () {
-              // Go to creator
-            },
-            icon: Icon(CupertinoIcons.pencil),
-            splashColor: Colors.transparent,
-          ),
-        ]
-      ),
+      appBar: MyAppBar.createWithAutoBack(context,
+          title: widget.asset.name,
+          onBack: () {},
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Go to creator
+              },
+              icon: Icon(CupertinoIcons.pencil),
+              splashColor: Colors.transparent,
+            ),
+          ]),
       body: Padding(
         padding: EdgeInsets.fromLTRB(
           AppStylesPrimary.safeAreaX,
@@ -56,14 +52,16 @@ class _CryptoDetailsPageState extends State<CryptoDetailsPage> {
                   height: 100,
                   child: imageUrl != null
                       ? ClipRRect(
-                    borderRadius: BorderRadius.circular(90.0),
-                    // child: Image.network(imageUrl),
-                    child: CachedNetworkImage(
-                      imageUrl: imageUrl,
-                      placeholder: (context, url) => SpinKitDoubleBounce(color: Colors.amber, size: 100),
-                      errorWidget: (context, url, error) => Icon(CupertinoIcons.cube, size: 100),
-                    ),
-                  )
+                          borderRadius: BorderRadius.circular(90.0),
+                          // child: Image.network(imageUrl),
+                          child: CachedNetworkImage(
+                            imageUrl: imageUrl,
+                            placeholder: (context, url) => SpinKitDoubleBounce(
+                                color: Colors.amber, size: 100),
+                            errorWidget: (context, url, error) =>
+                                Icon(CupertinoIcons.cube, size: 100),
+                          ),
+                        )
                       : Icon(CupertinoIcons.cube, size: 100),
                 ),
                 SizedBox(width: AppStylesPrimary.safeAreaX),
@@ -77,132 +75,147 @@ class _CryptoDetailsPageState extends State<CryptoDetailsPage> {
                 ),
               ],
             ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Text(
-                  "Description",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Flexible(
-                  child: Column(
+            widget.asset.description.isNotEmpty
+                ? Column(
                     children: [
-                      Text(
-                        widget.asset.description,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
-                        ),
-                        textAlign: TextAlign.left,
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text(
+                            "Description",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Flexible(
+                            child: Column(
+                              children: [
+                                Text(
+                                  widget.asset.description,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ],
-                  ),
-                ),
-              ],
-            ),
-
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Text(
-                  "Video",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-
-            SizedBox(height: 10),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Container(
-                color: Colors.deepPurpleAccent,
-                height: 150,
-              ),
-            ),
-
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Text(
-                  "Event",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Latitude',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-                Text(
-                  event?.latitude ?? '',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Longitude',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-                Text(
-                  event?.longitude ?? '',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Text(
-                  event?.note ?? '',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
+                  )
+                : Container(),
+            videoUrl != null
+                ? Column(
+                    children: [
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text(
+                            "Video",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Container(
+                          color: Colors.deepPurpleAccent,
+                          height: 150,
+                        ),
+                      ),
+                    ],
+                  )
+                : Container(),
+            event != null
+                ? Column(
+                    children: [
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Text(
+                            "Event",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Latitude',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            event.latitude,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Longitude',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            event.longitude,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            event.note,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                : Container(),
           ],
         ),
       ),
