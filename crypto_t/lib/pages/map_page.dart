f93d19eabad7fd45a57c03b76df4b512b3e1aae6
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-05-04 03:55:13 +0300

    Update map_page.dart

--- crypto_t/lib/pages/map_page.dart
+++ crypto_t/lib/pages/map_page.dart
@@ -1,5 +1,8 @@
+import 'package:crypto_t/apis/session.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'app_routes.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -10,16 +13,45 @@ class _MapPageState extends State<MapPage> {
 
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(0, 0);
+  final Set<Marker> _markers = Set();
 
-  void _onMapCreated(GoogleMapController controller) {
+  void updateMarkers() {
+    _markers.clear();
+    Session.shared.getLocalAssets()?.forEach((asset) {
+      var event = asset.suggestedEvent;
+      if (event != null) {
+        final marker = Marker(
+          markerId: MarkerId(asset.id),
+          position: LatLng(
+            double.parse(event.latitude),
+            double.parse(event.longitude),
+          ),
+          infoWindow: InfoWindow(
+            title: asset.name,
+            snippet: event.note,
+            onTap: () async {
+              await Navigator.pushNamed(context, AppRoutes.details, arguments: asset.id);
+            },
+          ),
+        );
+        _markers.add(marker);
+      }
+    });
+  }
+
+
+  Future<void> _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
+    setState(() {
+      updateMarkers();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
       onMapCreated: _onMapCreated,
+      markers: _markers,
       initialCameraPosition: CameraPosition(
         target: const LatLng(0, 0),
         zoom: 0.0,
