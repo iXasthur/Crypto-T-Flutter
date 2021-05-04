d6284725d3ab4c42aa6326a3e5941b7727602e28
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-05-04 03:41:51 +0300

    Update map_page.dart

--- crypto_t/lib/pages/map_page.dart
+++ crypto_t/lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -6,8 +7,23 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(0, 0);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Map");
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: const LatLng(0, 0),
+        zoom: 0.0,
+      ),
+    );
   }
 }
