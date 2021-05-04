83b2b0fd1c6cc10d49bfbb88dc62dd4bec327c0b
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-05-04 13:42:03 +0300

    Upd iOS support

--- crypto_t/ios/Runner/AppDelegate.swift
+++ crypto_t/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,15 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    if let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
+       let nsDictionary = NSDictionary(contentsOfFile: path),
+       let API_KEY = nsDictionary["API_KEY"] as? String {
+        GMSServices.provideAPIKey(API_KEY)
+    } else {
+        fatalError()
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
